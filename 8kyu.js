e87bda15df22f1ae51efde6b287330d404116107
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2025-12-19 16:17:14 +0500

    2-masala

--- 8kyu.js
+++ 8kyu.js
@@ -1,3 +1,18 @@
 // let maps = (x) => x.map((n)=>n*2)
 
 // console.log(maps([1, 2, 3]));
+
+// function sum (numbers) {
+//     if (numbers.length === 0) return 0;
+    
+//      let summa = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//       summa += numbers[i];
+//     }
+//     return summa
+//   }
+  
+//   console.log(sum([1, 5.2, 4, 0, -1]));  
+
+
+ 
