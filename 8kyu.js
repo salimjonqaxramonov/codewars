efb275a12176fee8020908161f19a2209af64b87
salimjonqaxramonov <qaxramonovsalimjon@gmail.com>
2026-01-22 19:02:07 +0500

    misol

--- 8kyu.js
+++ 8kyu.js
@@ -429,3 +429,13 @@
 //   };
 // console.log(cleanUp(2,2, [   ['_','_','@'],    ['@','_','_']  ]) )
 
+// function positiveSum(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       sum += arr[i];
+//     }
+//   }
+//   return sum;
+// }
+// console.log(positiveSum([1,2,3,4,5]));
